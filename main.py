d7eb655d928ccbfd310fb2f89c46a23a7f0b1008
Raymond <raytbrien@gmail.com>
2024-09-29 19:01:50 +0100

    Add basic file structure

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+from multi_page import MultiPage
+from template import show_page
+
+def __init__(self):
+    __name__ = '__main__'
+
+app = MultiPage('My App')
+app.app_page("Receipt", show_page)
+
+if __name__ == '__main__':
+    app.run()
---
+++ template.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_page():
+
+    st.info('Hello')
